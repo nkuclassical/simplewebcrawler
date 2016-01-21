7c5d246cea0243de645359ea3d12c92ddc491de0
Yanqiao <yzhan14@stevens.edu>
2016-01-20 22:42:00 -0500

    init

---
+++ src/webcrawler/URLReader.java
@@ -0,0 +1,106 @@
+package webcrawler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class URLReader {
+	Queue<String> queue = new LinkedList();
+	HashSet<String> set = new HashSet();
+	Path urlfile;
+
+	public URLReader() {
+		urlfile = Paths.get("urlfile");
+
+	}
+
+	public void getURLs(String input) {
+		for (int i = 0; i < input.length();) {
+			if (i + 7 < input.length()
+					&& input.substring(i, i + 7).equals("http://")
+					|| (i + 8 < input.length() && input.substring(i, i + 8)
+							.equals("https://"))) {
+				int r = i + 7;
+				while (r < input.length()) {
+					char c = input.charAt(r);
+					if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z'
+							|| c >= '0' && c <= '9' || c == '/' || c == '.'
+							|| c == '_' || c == '-')
+						r++;
+					else
+						break;
+				}
+				if (!set.contains(input.substring(i, r))) {
+					set.add(input.substring(i, r));
+					queue.add(input.substring(i, r));
+					String tempstring = input.substring(i, r) + '\n';
+					try {
+						Files.write(urlfile, tempstring.getBytes(),
+								StandardOpenOption.APPEND,
+								StandardOpenOption.CREATE);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+					}
+				}
+				i = r + 1;
+			} else {
+				i++;
+			}
+		}
+	}
+
+	public void URLreader(String rawurl) {
+		try {
+			URL url = new URL(rawurl);
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					url.openStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				getURLs(inputLine);
+			}
+			in.close();
+
+			System.out.println(rawurl + "   queue size:" + queue.size());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			// do nothing
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			// do nothing
+		} catch (IllegalArgumentException e) {
+			// do nothing
+		} catch (Exception e) {
+			// do nothing
+		}
+	}
+
+	public void start(String seed) {
+		queue.add(seed);
+		while (!queue.isEmpty()) {
+			URLreader(queue.poll());
+		}
+	}
+
+	public void writetoFile(String url) {
+
+	}
+
+	public static void main(String[] args) {
+		URLReader test = new URLReader();
+		String seed = "http://www.amazon.com/";
+
+		test.start(seed);
+	}
+}
